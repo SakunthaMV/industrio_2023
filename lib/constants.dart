cdd29ec5503f91fa72a7e5ed4754fb7651d1070b
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-09-07 17:18:43 +0530

    create constants file and add some constatnt values.

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+// Images
+
+const String logo = 'images/logo.png';
+
+// Menu
+
+const List<String> menuItems = [
+  'Home',
+  'Schedule',
+  'People',
+  'Projects',
+  'Posters',
+  'Proceedings',
+  'Contact Us',
+];
