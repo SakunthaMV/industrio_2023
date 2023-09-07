44a331ad80529b2d33997261110413fdf4f7a323
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-09-07 18:12:51 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:industrio_2023/pages/home/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyA50MvN9UizyjpNzhXsSBuV__-ANQ_EIrY",
+      projectId: "industrio-2023",
+      messagingSenderId: "719376501367",
+      appId: "1:719376501367:web:019ed3f28250c51cdcd472",
+    ),
+  );
   runApp(const MyApp());
 }
 
