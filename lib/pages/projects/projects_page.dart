d22c03991572f83213efee2f9202b371730ddb63
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-09-13 22:33:17 +0530

    cretate other 4 pages structure.

---
+++ lib/pages/projects/projects_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:industrio_2023/pages/widgets/common_page.dart';
+
+class ProjectsPage extends StatelessWidget {
+  static const String route = "Projects";
+  const ProjectsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CommonPage(
+      content: Placeholder(),
+    );
+  }
+}
---
+++ lib/pages/schedule/schedule_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:industrio_2023/pages/widgets/common_page.dart';
+
+class SchedulePage extends StatelessWidget {
+  static const String route = 'Schedule';
+  const SchedulePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CommonPage(
+      content: Placeholder(),
+    );
+  }
+}
