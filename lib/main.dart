8e4665b34e778ae564f4913c555937876895db4f
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-09-14 00:17:36 +0530

    add organizing commitee page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:industrio_2023/pages/home/home_page.dart';
+import 'package:industrio_2023/pages/people/organizing_commitee.dart';
 import 'package:industrio_2023/pages/people/people_page.dart';
 import 'package:industrio_2023/pages/posters/posters_page.dart';
 import 'package:industrio_2023/pages/proceeding/proceeding_page.dart';
@@ -54,6 +55,7 @@ class MyApp extends StatelessWidget {
         ProjectsPage.route: (context) => const ProjectsPage(),
         PostersPage.route: (context) => const PostersPage(),
         ProceedingPage.route: (context) => const ProceedingPage(),
+        OrganizingCommitee.route: (context) => const OrganizingCommitee(),
       },
     );
   }
